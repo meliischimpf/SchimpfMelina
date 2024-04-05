aba109ec49891b1c7c0ddd7c0c0891b86091dc3e
meliischimpf <melischimpf10@gmail.com>
2024-04-05 19:59:56 -0300

    tp1

---
+++ tp1/fn.js
@@ -0,0 +1,8 @@
+let frutas = ['pera', 'manzana', 'frutilla', 'banana', 'kiwi'];
+    for (const fruta of frutas) {
+        if (fruta == 'manzana') {
+            console.log(fruta);
+        }
+    }
+    
+
