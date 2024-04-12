fabeb663a67e45c3c7b1ccb8edf9bbf2c691d1d2
meliischimpf <melischimpf10@gmail.com>
2024-04-11 21:33:24 -0300

    condicion en el buscador

--- tp2/JS/functions.js
+++ tp2/JS/functions.js
@@ -5,17 +5,39 @@ function Personajes() {
     .then(res => res.json())
     .then(data => {
       const ryminfo = document.getElementById("ryminfo");
-      ryminfo.innerHTML = `
-        
-      <div class= "box">
-          <h2 class="name"><b>${data.name}</b></h2>
-        </div>
+      
 
+
+      let gender = data.gender;
+
+      if (gender === "unknown") 
+      then = ryminfo.innerHTML = `
+        
+      <div class= "picture-box">
+        <img src="${data.image}" alt="foto">
+      </div>
+
+      <div class= "info-box">
+          <h2 class="name"><b>${data.name}</b></h2>
+          <h4 class="gender">gender = ${data.gender}</h4>
+        </div>
       `;
+
+
+
+      if (gender != "unknown") 
+      then = ryminfo.innerHTML = `
+
+      <div class= "info-box">
+          <h2 class="name"><b>No es compatible</b></h2>
+        </div>
+      `;
+
     })
 
     .catch(error => {
       console.error('Error:', error);
       
     });
+
 }
