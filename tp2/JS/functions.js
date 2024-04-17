298960aaf2964a35d4d3379b6bc05a14545cd574
meliischimpf <melischimpf10@gmail.com>
2024-04-17 06:02:38 -0300

    Cambios
    Modifique el código debido a que me lo limitaba a 20 personajes.

--- tp2/JS/functions.js
+++ tp2/JS/functions.js
@@ -1,10 +1,6 @@
-  function Personajes() {
-  
+function Personajes() {
   const personaje1 = document.getElementById("personaje1").value;
   const personaje2 = document.getElementById("personaje2").value;
-  const box1 = document.getElementById("box1");
-  const box2 = document.getElementById("box2");
-  const match = document.getElementById("match");
 
   let genero1 = "";
   let genero2 = "";
@@ -12,78 +8,82 @@
   let nombre2 = "";
 
 
-  
-  fetch(`https://rickandmortyapi.com/api/character/`)
+
+  fetch(`https://rickandmortyapi.com/api/character/${personaje1},${personaje2}`)
     .then(res => res.json())
     .then(data => {
-      
-      console.log(data.results);
-      data.results.forEach(function(genero) {
 
+      const data1 = data[0];
+      const data2 = data[1];
 
-          if(personaje1 == genero.id){
-              box1.innerHTML = `
+      const box1 = document.getElementById("box1");
+      box1.innerHTML = `
+        
+      <div class="picture-box1">
+          <img src= ${data1.image}>
+      </div>
 
-              <div class="picture-box1">
-                <img src= ${genero.image}>
-              </div>
-
-              <div class="info-box1"> 
-                <h2>Nombre: ${genero.name}</h2>
-                <p>Especie: ${genero.species}</p>
-                <p>Estado: ${genero.status}</p>
-                <p>Genero: ${genero.gender}</p>
-              </div>
+      <div class="info-box1"> 
+          <h2>${data1.name}</h2>
+          <p>Especie: ${data1.species}</p
+          <p>Estado: ${data1.status}</p>
+          <p>Genero: ${data1.gender}</p>
+      </div>
               
-              `;
+        `;
 
-                  genero1 = genero.gender;
-                  nombre1 = genero.name;
+                  genero1 = data1.gender;
+                  nombre1 = data1.name;
+    
 
-              }
+
+    const box2 = document.getElementById("box2");
+    box2.innerHTML = `
+        
+      <div class="picture-box1">
+          <img src= ${data2.image}>
+      </div>
+
+      <div class="info-box1"> 
+          <h2>${data2.name}</h2>
+          <p>Especie: ${data2.species}</p
+          <p>Estado: ${data2.status}</p>
+          <p>Genero: ${data2.gender}</p>
+      </div>
               
+        `;
 
-          if(personaje2 == genero.id){
-              box2.innerHTML = `
-              
-              <div class="picture-box2">
-                <img src= ${genero.image}>
-              </div>
+                  genero2 = data2.gender;
+                  nombre2 = data2.name;
+                
 
-              <div class="info-box2"> 
-                <h2>Nombre: ${genero.name}</h2>
-                <p>Especie: ${genero.species}</p>
-                <p>Estado: ${genero.status}</p>
-                <p>Genero: ${genero.gender}</p>
-              </div>
-              
-              `;
 
-                genero2 = genero.gender;
-                nombre2 = genero.name;
+    const match = document.getElementById("match");
 
-              }
-
-            });
-
-          if (genero1 == genero2) {
+    if (genero1 == genero2) {
             match.innerHTML = `
+            
+            <h3 class="match-info"> Matching c: </h3>
             <img class="match-pic" src="js/img/match.png" alt="match">
-             <h3 class="match-info"> Matching c: </h3>
+          
             `;
+
 
         } else {
             match.innerHTML = `
-      
-            <img class="no-match-pic" src="js/img/no_match.png" alt="no match">
+
             <h3 class="no-match-info"> Not Matching :c </h3>
+            <img class="no-match-pic" src="js/img/no_match.png" alt="no match">
+            
             `;
     
-    }})
 
-    .catch (error => {
+      }})
+
+    .catch(error => {
       console.error('Error:', error);
       
-    });
+    })
 
 }
+
