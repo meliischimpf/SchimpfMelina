aa130197c173e8c5975d0d35d5b50d9e17bdf4b4
meliischimpf <melischimpf10@gmail.com>
2024-04-18 20:44:56 -0300

    cambios pj1 = pj2

--- tp2/JS/functions.js
+++ tp2/JS/functions.js
@@ -2,10 +2,8 @@ function Personajes() {
   const personaje1 = document.getElementById("personaje1").value;
   const personaje2 = document.getElementById("personaje2").value;
 
-  let genero1 = "";
-  let genero2 = "";
-  let nombre1 = "";
-  let nombre2 = "";
+  let generopj1 = "";
+  let generopj2 = "";
 
 
 
@@ -13,63 +11,101 @@ function Personajes() {
     .then(res => res.json())
     .then(data => {
 
-      const data1 = data[0];
-      const data2 = data[1];
+      const pj1 = data[0];
+      const pj2 = data[1];
+      
+      if (personaje1 === personaje2) {
+        const box1 = document.getElementById("box1");
+      box1.innerHTML = `
+        
+      <div class="picture-box1">
+          <img src= ${pj1.image}>
+      </div>
+
+      <div class="info-box1"> 
+          <h2>${pj1.name}</h2>
+          <p>Especie: ${pj1.species}</p
+          <p>Estado: ${pj1.status}</p>
+          <p>Genero: ${pj1.gender}</p>
+      </div>
+              
+        `;
+
+          generopj1 = pj1.gender;
+        
+        
+        const box2 = document.getElementById("box2");
+        box2.innerHTML = `
+            
+          <div class="picture-box2">
+              <img src= ${pj1.image}>
+          </div>
+    
+          <div class="info-box2"> 
+              <h2>${pj1.name}</h2>
+              <p>Especie: ${pj1.species}</p
+              <p>Estado: ${pj1.status}</p>
+              <p>Genero: ${pj1.gender}</p>
+          </div>
+                
+            `;
+
+            generopj2 = pj1.gender;
+
+      }
+
+      else {
 
       const box1 = document.getElementById("box1");
       box1.innerHTML = `
         
       <div class="picture-box1">
-          <img src= ${data1.image}>
+          <img src= ${pj1.image}>
       </div>
 
       <div class="info-box1"> 
-          <h2>${data1.name}</h2>
-          <p>Especie: ${data1.species}</p
-          <p>Estado: ${data1.status}</p>
-          <p>Genero: ${data1.gender}</p>
+          <h2>${pj1.name}</h2>
+          <p>Especie: ${pj1.species}</p
+          <p>Estado: ${pj1.status}</p>
+          <p>Genero: ${pj1.gender}</p>
       </div>
               
         `;
 
-                  genero1 = data1.gender;
-                  nombre1 = data1.name;
-    
+          generopj1 = pj1.gender;
 
-
-    const box2 = document.getElementById("box2");
     box2.innerHTML = `
         
-      <div class="picture-box1">
-          <img src= ${data2.image}>
+      <div class="picture-box2">
+          <img src= ${pj2.image}>
       </div>
 
-      <div class="info-box1"> 
-          <h2>${data2.name}</h2>
-          <p>Especie: ${data2.species}</p
-          <p>Estado: ${data2.status}</p>
-          <p>Genero: ${data2.gender}</p>
+      <div class="info-box2"> 
+          <h2>${pj2.name}</h2>
+          <p>Especie: ${pj2.species}</p
+          <p>Estado: ${pj2.status}</p>
+          <p>Genero: ${pj2.gender}</p>
       </div>
               
         `;
 
-                  genero2 = data2.gender;
-                  nombre2 = data2.name;
-                
+                  generopj2 = pj2.gender;
+
+    }
 
 
     const match = document.getElementById("match");
 
-    if (genero1 == genero2) {
+    if (generopj1 == generopj2) {
             match.innerHTML = `
             
             <h3 class="match-info"> Matching c: </h3>
             <img class="match-pic" src="js/img/match.png" alt="match">
           
             `;
-
-
-        } else {
+    } 
+        
+    else {
             match.innerHTML = `
 
             <h3 class="no-match-info"> Not Matching :c </h3>
@@ -77,8 +113,8 @@ function Personajes() {
             
             `;
     
-
-      }})
+  
+  }})
 
     .catch(error => {
       console.error('Error:', error);
