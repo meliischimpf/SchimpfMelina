9516d2b4a54289edb500b4e02f117cb6a2ff6866
meliischimpf <melischimpf10@gmail.com>
2024-04-19 20:52:10 -0300

    pasaje de url a archivo

--- tp3/JS/functions.js
+++ tp3/JS/functions.js
@@ -10,6 +10,9 @@ fetch ('https://randomuser.me/api/')
 
         console.log(datos);
 
+        let lat = datos.location.coordinates.latitude;
+        let lon = datos.location.coordinates.longitude;
+
         personaje.innerHTML = `
 
         <div class="picture-box">
@@ -20,33 +23,35 @@ fetch ('https://randomuser.me/api/')
             <h1>${datos.name.last}, ${ datos.name.first} </h1>
             <h3>Género: ${datos.gender}</h3>
             <h3>DNI: ${datos.id.value}</h3>
-            <h3>Longitud: ${datos.location.coordinates.latitude}</h3>
-            <h3>Latitud: ${datos.location.coordinates.longitude}</h3>
+            <h3>Longitud: ${lon}</h3>
+            <h3>Latitud: ${lat}</h3>
         </div>
            
             `;
 
-        let lat = datos.location.coordinates.latitude;
-        let lon = datos.location.coordinates.longitude;
-  
+
+
+        /*--------------------------------------------- mapa ------------------------------------------*/
+
+
+
         var mapa = L.DomUtil.get('mapa');
-            if (mapa != null) {
+             if (mapa != null) {
                 mapa._leaflet_id = null;
             }
 
-
         var map = L.map('mapa').setView([lat, lon], 3);
-
 
         L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
           maxZoom: 19,
           attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',})
           .addTo(map);
     
-
           L.marker([lat, lon]).addTo(map);
 
-        /**/   
+
+/* ------------------------------------------------- error ------------------------------------------------------------- */
+           
 
     })
        
