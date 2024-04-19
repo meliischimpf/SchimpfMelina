a02116dd65d26fa65e7654f78483fb25c67c844e
meliischimpf <melischimpf10@gmail.com>
2024-04-19 04:04:26 -0300

    conexión con las apis

--- tp3/JS/functions.js
+++ tp3/JS/functions.js
@@ -1,36 +1,59 @@
 function Busqueda() {
+
 const personaje = document.getElementById("personaje");
 
+fetch ('https://randomuser.me/api/')
+.then(res => res.json())
+.then(data => {
 
-fetch( 'https://randomuser.me/api/')
-.then( (response) => {response.json})
-.then( (data) => {
+        const datos = data.results[0];
 
-        console.log(data);
+        console.log(datos);
 
+        personaje.innerHTML = `
 
-})}  
+        <div class="picture-box">
+            <img src= ${datos.picture.large}>
+        </div>
 
+        <div class="info-box"> 
+            <h1>${datos.name.last}, ${ datos.name.first} </h1>
+            <h3>Género: ${datos.gender}</h3>
+            <h3>DNI: ${datos.id.value}</h3>
+            <h3>Longitud: ${datos.location.coordinates.latitude}</h3>
+            <h3>Latitud: ${datos.location.coordinates.longitude}</h3>
+        </div>
+           
+            `;
 
-/*
-     personaje.innerHTML = `
-    
-    <div class="picture-box">
-          <img src= ${data.picture}>
-      </div>
-
-      <div class="info-box"> 
-          <h2>${data.results.0.name}</h2>
-          <p>Genero: ${data.gender}</p>
-      </div>   
-      `*/
-
-
-    
-    
+        let lat = datos.location.coordinates.latitude;
+        let lon = datos.location.coordinates.longitude;
   
-        
+        var mapa = L.DomUtil.get('mapa');
+            if (mapa != null) {
+                mapa._leaflet_id = null;
+            }
+
+
+        var map = L.map('mapa').setView([lat, lon], 3);
+
+
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          maxZoom: 19,
+          attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',})
+          .addTo(map);
     
-   
-    
-  
+
+          L.marker([lat, lon]).addTo(map);
+
+        /**/   
+
+    })
+       
+
+        .catch((error) => {
+            console.error('Error:', error);
+
+     });
+
+}
