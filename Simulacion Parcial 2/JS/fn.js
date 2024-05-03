67f12d0ddfbd81a3db1f44f8167a0b6b4a93dfca
meliischimpf <melischimpf10@gmail.com>
2024-05-03 20:48:57 -0300

    arreglos

--- Simulacion Parcial 2/JS/fn.js
+++ Simulacion Parcial 2/JS/fn.js
@@ -6,61 +6,130 @@ let array2 = [];
 
 function personaje1() {
     const personaje1 = document.getElementById("personaje1").value;
-
-    array.push(personaje1);
-
     const boton1 = document.getElementById('boton1');
     
+if (personaje1 < 827 && personaje1 > 0){
+
+  array.push(personaje1);
 
     if (array.length == 3) {
+
         
         boton1.disabled = true;
 
-        var pj1 = array [0];
-        var pj2 = array [1];
-        var pj3 = array [2];
-            
-        console.log (pj1, pj2, pj3);
-        mostrarpersonaje1 (pj1, pj2, pj3);
+        var pj1 = array [0]; 
+        var pj2 = array [1];  
+        var pj3 = array [2];  
 
+        
+        console.log (array);
+        mostrarpersonaje (array);
+      }
     }
+
+    else {
+      alert ("Error: Valor incorrecto");
+    }
+
 }
 
+
+
 function personaje2() {
+
     const personaje2 = document.getElementById("personaje2").value;
-
-    array2.push(personaje2);
-
     const boton2 = document.getElementById('boton2');
+
+if (personaje2 < 827 && personaje2 > 0){
+
+  array2.push(personaje2);
 
     if (array2.length == 3) {
         
         boton2.disabled = true;
         
-        var pj4 = array2 [0];
-        var pj5 = array2 [1];
-        var pj6 = array2 [2];
-            
-        console.log (pj4, pj5, pj6);
-        mostrarpersonaje2 (pj4, pj5, pj6);
+        var pj4 = array2 [0]; 
+        var pj5 = array2 [1]; 
+        var pj6 = array2 [2]; 
 
-        }  
-         
+        let arraysuma = array.concat(array2);
+        console.log (arraysuma);
+        mostrarpersonaje (arraysuma);
+          
+      }   
+    }  
+  
+  else {
+    alert ("Error: Valor incorrecto");
+  }
+   }   
+    
+
+    function mostrarpersonaje(arraysuma) {
+
+      
+
+      fetch(`https://rickandmortyapi.com/api/character/`+ arraysuma)
+        .then(res => res.json())
+        .then(data => {
+  
+  
+              const box1 = document.getElementById("box1");
+              const box2 = document.getElementById("box2");
+
+
+          if (arraysuma.length == 6){
+            console.log (arraysuma);
+              
+            for (i = 0; i <= 6; i++){
+                if (data[i] == arraysuma[i]){
+                  
+                    
+                    box1.innerHTML = `
+                      <div class="picture-box1">
+                          <img src= ${data[i].image}>
+                          <img src= ${data[i].image}>
+                          <img src= ${data[i].image}>
+                      </div>
+                      `;
+                      
+                    box2.innerHTML = `
+                      <div class="picture-box2">
+                          <img src= ${data[i].image}>
+                          <img src= ${data[i].image}>
+                          <img src= ${data[i].image}>
+                      </div>
+                      `;
+                      }
+
+
+
+              }
+    
+              
+
+    }
+  })
 }
+      
 
 
+    /*
   function mostrarpersonaje1(pj1, pj2, pj3) {
 
     fetch(`https://rickandmortyapi.com/api/character/${pj1},${pj2},${pj3}`)
       .then(res => res.json())
       .then(data => {
 
+
             const box1 = document.getElementById("box1");
-            
+             
             var pj1= data[0];
             var pj2= data[1];
             var pj3= data[2];
+           
             
+
             box1.innerHTML = `
             <div class="picture-box1">
                 <img src= ${pj1.image}>
@@ -68,12 +137,13 @@ function personaje2() {
                 <img src= ${pj3.image}>
             </div>
             `;
+          })
+      }
 
-      })
+    
 
-        .catch(error => console.log(error));
-        document.getElementById("error").innerHTML = "error: " + [error];
-    }
+
+
 
       function mostrarpersonaje2(pj4, pj5, pj6) {
 
@@ -87,6 +157,7 @@ function personaje2() {
                 var pj5= data[1];
                 var pj6= data[2];
     
+                
                 box2.innerHTML = `
                 <div class="picture-box1">
                     <img src= ${pj4.image}>
@@ -95,16 +166,9 @@ function personaje2() {
                 </div>
                 `;
           })
-
-            .catch(error => console.log(error));
-            document.getElementById("error").innerHTML = "error: " + [error];
-    }
-
-    
-
-                 
-    
-
+        }
+      
+*/
 
 
 
