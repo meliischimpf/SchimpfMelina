fadeebe0fb2290af3692b3129a9748c8eb0930d5
meliischimpf <melischimpf10@gmail.com>
2024-05-09 20:19:24 -0300

    arreglitos

--- Simulacion Parcial 2/JS/fn.js
+++ Simulacion Parcial 2/JS/fn.js
@@ -1,5 +1,7 @@
 var personajes1 = 0;
 var personajes2 = 0;
+var primero = 1;
+var ultimo = 826;
 let array = [];
 let array2 = [];
 
@@ -8,22 +10,15 @@ function personaje1() {
     const personaje1 = document.getElementById("personaje1").value;
     const boton1 = document.getElementById('boton1');
     
-if (personaje1 < 827 && personaje1 > 0){
+  if (personaje1 <= ultimo && personaje1 >= primero){
 
   array.push(personaje1);
 
     if (array.length == 3) {
 
-        
         boton1.disabled = true;
-
-        var pj1 = array [0]; 
-        var pj2 = array [1];  
-        var pj3 = array [2];  
-
         
         console.log (array);
-        mostrarpersonaje (array);
       }
     }
 
@@ -40,19 +35,15 @@ function personaje2() {
     const personaje2 = document.getElementById("personaje2").value;
     const boton2 = document.getElementById('boton2');
 
-if (personaje2 < 827 && personaje2 > 0){
+  if (personaje2 < 827 && personaje2 > 0){
 
   array2.push(personaje2);
 
     if (array2.length == 3) {
         
         boton2.disabled = true;
-        
-        var pj4 = array2 [0]; 
-        var pj5 = array2 [1]; 
-        var pj6 = array2 [2]; 
 
-        let arraysuma = array.concat(array2);
+        arraysuma = array.concat(array2);
         console.log (arraysuma);
         mostrarpersonaje (arraysuma);
           
@@ -62,118 +53,60 @@ if (personaje2 < 827 && personaje2 > 0){
   else {
     alert ("Error: Valor incorrecto");
   }
-   }   
+}   
     
 
-    function mostrarpersonaje(arraysuma) {
+function mostrarpersonaje(arraysuma) {
 
-      
+     try {
 
       fetch(`https://rickandmortyapi.com/api/character/`+ arraysuma)
         .then(res => res.json())
         .then(data => {
-  
-  
-              const box1 = document.getElementById("box1");
-              const box2 = document.getElementById("box2");
+      
+      
+          const box1 = document.getElementById("box1");
+          const box2 = document.getElementById("box2");
 
 
-          if (arraysuma.length == 6){
-            console.log (arraysuma);
-              
-            for (i = 0; i <= 6; i++){
-                if (data[i] == arraysuma[i]){
-                  
-                    
-                    box1.innerHTML = `
-                      <div class="picture-box1">
-                          <img src= ${data[i].image}>
-                          <img src= ${data[i].image}>
-                          <img src= ${data[i].image}>
-                      </div>
-                      `;
-                      
-                    box2.innerHTML = `
-                      <div class="picture-box2">
-                          <img src= ${data[i].image}>
-                          <img src= ${data[i].image}>
-                          <img src= ${data[i].image}>
-                      </div>
-                      `;
+                  console.log (data);
+
+                for (i = 0; i < data.length; i++){
+                  for (j = 0; j < array.length; j++){ 
+                
+                        if (data[i].id == array[j]) {
+                          
+                          box1.innerHTML += `
+                          <div class="picture-box1">
+                              <img src= ${data[i].image}>
+                          </div>
+                          `;
+                        }
+                        }
                       }
 
+                      
+                  for (i = 0; i < data.length; i++){
+                      for (k = 0; k < array2.length; k++){ 
+          
+                        if (data[i].id == array2[k]) {
+                        
+                            box2.innerHTML += `
+                          <div class="picture-box2">
+                              <img src=${data[i].image}>
+                          </div>
+                          `;
+                          
+                        }
 
-
-              }
-    
-              
-
-    }
-  })
-}
-      
-
-
-    /*
-  function mostrarpersonaje1(pj1, pj2, pj3) {
-
-    fetch(`https://rickandmortyapi.com/api/character/${pj1},${pj2},${pj3}`)
-      .then(res => res.json())
-      .then(data => {
-
-
-            const box1 = document.getElementById("box1");
-             
-            var pj1= data[0];
-            var pj2= data[1];
-            var pj3= data[2];
-           
-            
-
-            box1.innerHTML = `
-            <div class="picture-box1">
-                <img src= ${pj1.image}>
-                <img src= ${pj2.image}>
-                <img src= ${pj3.image}>
-            </div>
-            `;
-          })
-      }
-
-    
-
-
-
-
-      function mostrarpersonaje2(pj4, pj5, pj6) {
-
-        fetch(`https://rickandmortyapi.com/api/character/${pj4},${pj5},${pj6}`)
-          .then(res => res.json())
-          .then(data => {
-
-                const box2 = document.getElementById("box2");
-                
-                var pj4= data[0];
-                var pj5= data[1];
-                var pj6= data[2];
-    
-                
-                box2.innerHTML = `
-                <div class="picture-box1">
-                    <img src= ${pj4.image}>
-                    <img src= ${pj5.image}>
-                    <img src= ${pj6.image}>
-                </div>
-                `;
-          })
+                  }
+           }
         }
+      )
+    } 
+      catch (error) {
       
-*/
-
-
-
-
-            
-           
-   
-    
+      }{ 
+}
+}
+          
