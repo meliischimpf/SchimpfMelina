c9636b1790de8373a5f64d09b9732f6fabded386
meliischimpf <melischimpf10@gmail.com>
2024-05-17 20:45:41 -0300

    busquedaapi

--- poo/segunda practica/functions.js
+++ poo/segunda practica/functions.js
@@ -12,32 +12,78 @@ class Persona {
         this.email = email;
     }
 
-        llamar(){
 
-            mostrar.innerHTML = `
+    getEdad(){
 
-            <div class="info-box"> 
-                <h1>${this.name}, ${this.lastname} </h1>
-                <h3>Género: ${this.gender}</h3>
-                <h3>Ciudad: ${this.city}</h3>
-                <h3>Email: ${this.email}</h3>
-            </div>
+        const fecha = new Date();
+
+        const anoActual = fecha.getFullYear();
+        const mesActual = fecha.getMonth() + 1; 
+        const diaActual = fecha.getDate();
+
+
         
-            `;
-        }
+        let ano = anoActual - this.fecha_nacimiento;
+            
+            if (mesActual <= this.fecha_nacimiento || diaActual < this.fecha_nacimiento) {
+                 ano = (ano - 1);
+            }
+
+    }
+
 }
 
 
+class Cliente extends Persona {
+    constructor (name, lastname, gender, city, email, ncuenta) {
+        super (name, lastname, gender, city, email);
 
-/* class Cliente extends Persona {
-    constructor (nombre, apellido, dni, nacionalidad, fecha_nacimiento, cuenta) {
-        super (nombre, apellido, dni, nacionalidad, fecha_nacimiento);
-
-        this.cuenta = cuenta;
+        this.ncuenta = ncuenta;
 
     }
+
+    llamarC(){
+
+        mostrar.innerHTML = `
+
+        <div class="info-box"> 
+            <h1>${this.name}, ${this.lastname} </h1>
+            <h3>Género: ${this.gender}</h3>
+            <h3>Ciudad: ${this.city}</h3>
+            <h3>Email: ${this.email}</h3>
+            <h3>Nº Cuenta: ${this.ncuenta}</h3>
+        </div>
+    
+        `;
+    }
+    
+}
+
+class Empleado extends Persona {
+    constructor (name, lastname, gender, city, email, legajo) {
+        super (name, lastname, gender, city, email);
+
+        this.legajo = legajo;
+
+    }
+
+
+    llamarE(){
+
+        mostrar.innerHTML = `
+
+        <div class="info-box"> 
+            <h1>${this.name}, ${this.lastname} </h1>
+            <h3>Género: ${this.gender}</h3>
+            <h3>Ciudad: ${this.city}</h3>
+            <h3>Email: ${this.email}</h3>
+            <h3>Legajo: ${this.legajo}</h3>
+        </div>
+    
+        `;
+    }
 }    
-*/
+
 
 function buscarPersona (){;
 
@@ -46,12 +92,25 @@ fetch ('https://randomuser.me/api/')
 .then(data => {
 
         const datos = data.results[0];
-        console.log(datos);
 
-        let persona1 = new Persona (datos.name.first, datos.name.last, datos.gender, datos.location.city, datos.email);
-        console.log (persona1);
+        age = datos.dob.age;
+
+        if (age < 18) {
+
+            let persona1 = new Cliente (datos.name.first, datos.name.last, datos.gender, datos.location.city, datos.email, datos.location.number);
+            console.log (persona1);
+            persona1.llamarC();
+        }
+        
+        else {
+
+            let persona1 = new Empleado (datos.name.first, datos.name.last, datos.gender, datos.location.city, datos.email, datos.location.postcode);
+            console.log (persona1);
+            persona1.llamarE();
+
+        }
        
-        persona1.llamar();
+   
 
 })
 }
