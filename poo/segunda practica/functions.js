fdea4dfc20512d97bba2582e47603ce4758adf09
meliischimpf <melischimpf10@gmail.com>
2024-05-17 20:17:28 -0300

    busquedaapi

--- poo/segunda practica/functions.js
+++ poo/segunda practica/functions.js
@@ -1,4 +1,4 @@
-// pantalla con un boton que sea buscar que este boton le pida a ala api de random user,
+// pantalla con un boton que sea buscar que este boton le pida a a la api de random user,
 // se recibe esa persona y se instancia una persona y muestren los datos de esa instancia.
 
 
@@ -10,11 +10,26 @@ class Persona {
         this.gender = gender;
         this.city = city;
         this.email = email;
-     }
+    }
 
+        llamar(){
+
+            mostrar.innerHTML = `
+
+            <div class="info-box"> 
+                <h1>${this.name}, ${this.lastname} </h1>
+                <h3>Género: ${this.gender}</h3>
+                <h3>Ciudad: ${this.city}</h3>
+                <h3>Email: ${this.email}</h3>
+            </div>
+        
+            `;
+        }
 }
 
-class Cliente extends Persona {
+
+
+/* class Cliente extends Persona {
     constructor (nombre, apellido, dni, nacionalidad, fecha_nacimiento, cuenta) {
         super (nombre, apellido, dni, nacionalidad, fecha_nacimiento);
 
@@ -22,9 +37,28 @@ class Cliente extends Persona {
 
     }
 }    
+*/
+
+function buscarPersona (){;
+
+fetch ('https://randomuser.me/api/')
+.then(res => res.json())
+.then(data => {
+
+        const datos = data.results[0];
+        console.log(datos);
+
+        let persona1 = new Persona (datos.name.first, datos.name.last, datos.gender, datos.location.city, datos.email);
+        console.log (persona1);
+       
+        persona1.llamar();
+
+})
+}
+
+    
 
 
-let melina = new Cliente ('Melina', 'Schimpf', 45338356, 'Argentina', 2003, 7);
-console.log (melina);
 
-melina.saludar();
+
+
