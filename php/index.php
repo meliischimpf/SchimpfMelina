2f9866c9d5364714bbf69d0d06f21ea7e45d2b04
meliischimpf <melischimpf10@gmail.com>
2024-06-28 20:27:46 -0300

    php

--- php/index.php
+++ php/index.php
@@ -11,9 +11,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    
-    , initial-scale=1.0">
+    <meta name="viewport" content="width=, initial-scale=1.0">
     <title>php</title>
 </head>
 <body>
